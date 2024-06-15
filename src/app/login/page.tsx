d9c278863c1a91face97ae048b5aaa4c498bc478
Aayush Adoni <aayushadoni1@gmail.com>
2024-06-15 14:33:05 +0530

    .

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -101,6 +101,8 @@ export default function Home() {
                                 onClick={async () => {
                                   const account = await metamask.connect({ client });
                                   setAccount(account)
+                                  console.log(account)
+                                  loginWithWallet()
                                 }}
                                 className="w-full max-w-xs font-bold shadow-sm rounded-lg py-2 bg-gray-100 hover:bg-gray-300 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
                                 <div className="">
